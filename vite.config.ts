e9e171b47b1fbd4d154a750c6e5c7258e7075454
Developer <developer@example.com>
2026-03-30 23:54:02 +0530

    Fix build config and restore all effects

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
